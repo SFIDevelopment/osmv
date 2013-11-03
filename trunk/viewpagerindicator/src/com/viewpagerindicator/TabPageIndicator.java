a07db21e85560c4d89bf017d0f41dba8aba12e75
furykid <furykid@c2d2dfc4-6688-a121-87d6-3d36b9c7bb10>
2013-11-03 18:06:00 +0000

    cleanup

--- trunk/viewpagerindicator/src/com/viewpagerindicator/TabPageIndicator.java
+++ trunk/viewpagerindicator/src/com/viewpagerindicator/TabPageIndicator.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project Copyright (C) 2011 Jake
- * Wharton
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2011 Jake Wharton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.viewpagerindicator;
 
@@ -21,39 +21,58 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 /**
  * This widget implements the dynamic action bar tab behavior that can change
  * across different configurations or circumstances.
  */
-public class TabPageIndicator extends HorizontalScrollView implements
-        PageIndicator {
-    Runnable                               mTabSelector;
+public class TabPageIndicator extends HorizontalScrollView implements PageIndicator {
+    /** Title text used when no title is provided by the adapter. */
+    private static final CharSequence EMPTY_TITLE = "";
 
-    private OnClickListener                mTabClickListener = new OnClickListener() {
-                                                                 public void onClick(
-                                                                         View view) {
-                                                                     TabView tabView = (TabView) view;
-                                                                     mViewPager
-                                                                             .setCurrentItem(tabView
-                                                                                     .getIndex());
-                                                                 }
-                                                             };
+    /**
+     * Interface for a callback when the selected tab has been reselected.
+     */
+    public interface OnTabReselectedListener {
+        /**
+         * Callback when the selected tab has been reselected.
+         *
+         * @param position Position of the current center item.
+         */
+        void onTabReselected(int position);
+    }
 
-    private LinearLayout                   mTabLayout;
-    private ViewPager                      mViewPager;
+    private Runnable mTabSelector;
+
+    private final OnClickListener mTabClickListener = new OnClickListener() {
+        public void onClick(View view) {
+            TabView tabView = (TabView)view;
+            final int oldSelected = mViewPager.getCurrentItem();
+            final int newSelected = tabView.getIndex();
+            mViewPager.setCurrentItem(newSelected);
+            if (oldSelected == newSelected && mTabReselectedListener != null) {
+                mTabReselectedListener.onTabReselected(newSelected);
+            }
+        }
+    };
+
+    private final IcsLinearLayout mTabLayout;
+
+    private ViewPager mViewPager;
     private ViewPager.OnPageChangeListener mListener;
 
-    private LayoutInflater                 mInflater;
+    private int mMaxTabWidth;
+    private int mSelectedTabIndex;
 
-    int                                    mMaxTabWidth;
-    private int                            mSelectedTabIndex;
+    private OnTabReselectedListener mTabReselectedListener;
 
     public TabPageIndicator(Context context) {
         this(context, null);
@@ -63,12 +82,12 @@ public class TabPageIndicator extends HorizontalScrollView implements
         super(context, attrs);
         setHorizontalScrollBarEnabled(false);
 
-        mInflater = LayoutInflater.from(context);
+        mTabLayout = new IcsLinearLayout(context, R.attr.vpiTabPageIndicatorStyle);
+        addView(mTabLayout, new ViewGroup.LayoutParams(WRAP_CONTENT, MATCH_PARENT));
+    }
 
-        mTabLayout = new LinearLayout(getContext());
-        addView(mTabLayout, new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.FILL_PARENT));
+    public void setOnTabReselectedListener(OnTabReselectedListener listener) {
+        mTabReselectedListener = listener;
     }
 
     @Override
@@ -78,10 +97,9 @@ public class TabPageIndicator extends HorizontalScrollView implements
         setFillViewport(lockedExpanded);
 
         final int childCount = mTabLayout.getChildCount();
-        if (childCount > 1
-                && (widthMode == MeasureSpec.EXACTLY || widthMode == MeasureSpec.AT_MOST)) {
+        if (childCount > 1 && (widthMode == MeasureSpec.EXACTLY || widthMode == MeasureSpec.AT_MOST)) {
             if (childCount > 2) {
-                mMaxTabWidth = (int) (MeasureSpec.getSize(widthMeasureSpec) * 0.4f);
+                mMaxTabWidth = (int)(MeasureSpec.getSize(widthMeasureSpec) * 0.4f);
             } else {
                 mMaxTabWidth = MeasureSpec.getSize(widthMeasureSpec) / 2;
             }
@@ -106,8 +124,7 @@ public class TabPageIndicator extends HorizontalScrollView implements
         }
         mTabSelector = new Runnable() {
             public void run() {
-                final int scrollPos = tabView.getLeft()
-                        - (getWidth() - tabView.getWidth()) / 2;
+                final int scrollPos = tabView.getLeft() - (getWidth() - tabView.getWidth()) / 2;
                 smoothScrollTo(scrollPos, 0);
                 mTabSelector = null;
             }
@@ -132,16 +149,18 @@ public class TabPageIndicator extends HorizontalScrollView implements
         }
     }
 
-    private void addTab(String text, int index) {
-        // Workaround for not being able to pass a defStyle on pre-3.0
-        final TabView tabView = (TabView) mInflater.inflate(R.layout.vpi__tab,
-                null);
-        tabView.init(this, text, index);
+    private void addTab(int index, CharSequence text, int iconResId) {
+        final TabView tabView = new TabView(getContext());
+        tabView.mIndex = index;
         tabView.setFocusable(true);
         tabView.setOnClickListener(mTabClickListener);
+        tabView.setText(text);
 
-        mTabLayout.addView(tabView, new LinearLayout.LayoutParams(0,
-                LayoutParams.FILL_PARENT, 1));
+        if (iconResId != 0) {
+            tabView.setCompoundDrawablesWithIntrinsicBounds(iconResId, 0, 0, 0);
+        }
+
+        mTabLayout.addView(tabView, new LinearLayout.LayoutParams(0, MATCH_PARENT, 1));
     }
 
     @Override
@@ -168,14 +187,15 @@ public class TabPageIndicator extends HorizontalScrollView implements
 
     @Override
     public void setViewPager(ViewPager view) {
+        if (mViewPager == view) {
+            return;
+        }
+        if (mViewPager != null) {
+            mViewPager.setOnPageChangeListener(null);
+        }
         final PagerAdapter adapter = view.getAdapter();
         if (adapter == null) {
-            throw new IllegalStateException(
-                    "ViewPager does not have adapter instance.");
-        }
-        if (!(adapter instanceof TitleProvider)) {
-            throw new IllegalStateException(
-                    "ViewPager adapter must implement TitleProvider to be used with TitlePageIndicator.");
+            throw new IllegalStateException("ViewPager does not have adapter instance.");
         }
         mViewPager = view;
         view.setOnPageChangeListener(this);
@@ -184,10 +204,22 @@ public class TabPageIndicator extends HorizontalScrollView implements
 
     public void notifyDataSetChanged() {
         mTabLayout.removeAllViews();
-        TitleProvider adapter = (TitleProvider) mViewPager.getAdapter();
-        final int count = ((PagerAdapter) adapter).getCount();
+        PagerAdapter adapter = mViewPager.getAdapter();
+        IconPagerAdapter iconAdapter = null;
+        if (adapter instanceof IconPagerAdapter) {
+            iconAdapter = (IconPagerAdapter)adapter;
+        }
+        final int count = adapter.getCount();
         for (int i = 0; i < count; i++) {
-            addTab(adapter.getTitle(i), i);
+            CharSequence title = adapter.getPageTitle(i);
+            if (title == null) {
+                title = EMPTY_TITLE;
+            }
+            int iconResId = 0;
+            if (iconAdapter != null) {
+                iconResId = iconAdapter.getIconResId(i);
+            }
+            addTab(i, title, iconResId);
         }
         if (mSelectedTabIndex > count) {
             mSelectedTabIndex = count - 1;
@@ -202,16 +234,14 @@ public class TabPageIndicator extends HorizontalScrollView implements
         setCurrentItem(initialPosition);
     }
 
-    public int getCurrentItem() {
-        return mViewPager.getCurrentItem();
-    }
-
     @Override
     public void setCurrentItem(int item) {
         if (mViewPager == null) {
             throw new IllegalStateException("ViewPager has not been bound.");
         }
         mSelectedTabIndex = item;
+        mViewPager.setCurrentItem(item);
+
         final int tabCount = mTabLayout.getChildCount();
         for (int i = 0; i < tabCount; i++) {
             final View child = mTabLayout.getChildAt(i);
@@ -222,26 +252,23 @@ public class TabPageIndicator extends HorizontalScrollView implements
             }
         }
     }
+    
+	@Override
+	public int getCurrentItem() {
+		return mSelectedTabIndex;
+	}
+
 
     @Override
     public void setOnPageChangeListener(OnPageChangeListener listener) {
         mListener = listener;
     }
 
-    public static class TabView extends LinearLayout {
-        private TabPageIndicator mParent;
-        private int              mIndex;
+    private class TabView extends TextView {
+        private int mIndex;
 
-        public TabView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-        }
-
-        public void init(TabPageIndicator parent, String text, int index) {
-            mParent = parent;
-            mIndex = index;
-
-            TextView textView = (TextView) findViewById(android.R.id.text1);
-            textView.setText(text);
+        public TabView(Context context) {
+            super(context, null, R.attr.vpiTabPageIndicatorStyle);
         }
 
         @Override
@@ -249,10 +276,8 @@ public class TabPageIndicator extends HorizontalScrollView implements
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
             // Re-measure if we went beyond our maximum size.
-            if (mParent.mMaxTabWidth > 0
-                    && getMeasuredWidth() > mParent.mMaxTabWidth) {
-                super.onMeasure(MeasureSpec.makeMeasureSpec(
-                        mParent.mMaxTabWidth, MeasureSpec.EXACTLY),
+            if (mMaxTabWidth > 0 && getMeasuredWidth() > mMaxTabWidth) {
+                super.onMeasure(MeasureSpec.makeMeasureSpec(mMaxTabWidth, MeasureSpec.EXACTLY),
                         heightMeasureSpec);
             }
         }
